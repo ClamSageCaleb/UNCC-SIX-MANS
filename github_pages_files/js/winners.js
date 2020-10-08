e2aee832ead63c34e3850ae2897a46c72ed029d4
mattwells19 <mwells21@uncc.edu>
2020-10-08 17:00:28 -0400

    Winners done and table search added

--- github_pages_files/js/winners.js
+++ github_pages_files/js/winners.js
@@ -1,46 +1,31 @@
 $(() => loadWinners())
 
-const winnerFiles = ["summer2020.json", "fall2020.json"];
+const winnerFiles = ["summer2020.json"]
 
 const loadWinners = async () => {
   for (const file of winnerFiles) {
-    const data = await fetch(`/github_pages_files/winners/${file}`).then(res => res.json());
-    console.log(data, file)
-    await getWinners(data, file.replace(".json", ""));
-  };
+    const data = await fetch(`/github_pages_files/winners/${file}`).then((res) => res.json())
+    data.forEach((player, index) => populateWinners(player, index + 1, file.replace(".json", "")))
+  }
 
-  console.log("accordion");
   $("#accordion").accordion({
-    active: false,
     collapsible: true,
     icons: { header: "accordionClosed", activeHeader: "accordionOpen" },
-    heightStyle: "content"
+    heightStyle: "content",
   })
 }
 
-async function getWinners(winnerData, tableId) {
-  // for (const )
-  winnerData.forEach(async (winnerDetails, index) => {
-    const data = await fetch(`https://discordapp.com/api/users/${winnerDetails.id}`, {
-      method: "GET",
-      headers: {
-        "Authorization": "Bot [token here]"
-      }
-    }).then(res => res.json());
-    populateWinners(data, winnerData[index], index + 1, tableId);
-  });
-
-}
-
-function populateWinners(winnerDiscordUser, winnerDetails, rank, tableId) {
+function populateWinners(winnerDetails, rank, tableId) {
   $(`#winners-table-${tableId} > tbody`).append(
     `<tr>
       <td headers="rank">${rank}</td>
-      <td headers="name">${winnerDiscordUser.username}#${winnerDiscordUser.discriminator}</td>
+      <td headers="name">${winnerDetails["Name"]}</td>
       <td headers="wins">${winnerDetails["Wins"]}</td>
       <td headers="losses">${winnerDetails["Losses"]}</td>
       <td headers="matches-played">${winnerDetails["Wins"] + winnerDetails["Losses"]}</td>
-      <td headers="win-perc">${parseInt((winnerDetails["Wins"] / (winnerDetails["Wins"] + winnerDetails["Losses"])) * 100)}%</td>
+      <td headers="win-perc">${parseInt(
+        (winnerDetails["Wins"] / (winnerDetails["Wins"] + winnerDetails["Losses"])) * 100
+      )}%</td>
     </tr>`
   )
 }
