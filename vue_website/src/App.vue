d1dc07394c46a8ddd426828a8cd7bcb3bef7f1bb
mattwells19 <mwells21@uncc.edu>
2020-10-17 17:01:36 -0400

    Start to vue website

---
+++ vue_website/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <NavBar />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+import NavBar from "./components/NavBar.vue"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    NavBar,
+  },
+})
+</script>
+
+<style>
+#app {
+  scroll-behavior: smooth;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0;
+}
+</style>
---
+++ vue_website/src/components/NavBar.vue
@@ -0,0 +1,123 @@
+<template>
+  <div>
+    <button id="open-sidebar" @click="state.open = true">
+      <img class="icon" src="../assets/open_sidebar.svg" />
+    </button>
+    <div class="sidebar-backdrop" @click="state.open = false" v-if="state.open"></div>
+    <transition name="slide">
+      <div id="side-nav" v-if="state.open">
+        <nav id="nav-container" class="center">
+          <button id="close-sidebar" @click="state.open = false"><img src="../assets/close.svg" /></button>
+          <a href="#">
+            <img class="icon" src="../assets/home.svg" />
+            Home
+          </a>
+          <a href="#commands">
+            <img class="icon" src="../assets/commands.svg" />
+            Commands
+          </a>
+          <a href="#hall-of-fame">
+            <img class="icon" src="../assets/hall_of_fame.svg" />
+            Hall of Fame
+          </a>
+          <a href="#leaderboard">
+            <img class="icon" src="../assets/leaderboard.svg" />
+            Leaderboard
+          </a>
+        </nav>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "NavBar",
+  data: () => {
+    return {
+      state: {
+        open: false,
+      },
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+button {
+  cursor: pointer;
+}
+#open-sidebar {
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: transparent;
+  border: none;
+  padding: 0;
+}
+.sidebar-backdrop {
+  background-color: rgba(0, 0, 0, 0.5);
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  cursor: pointer;
+}
+#side-nav {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: min-content;
+  background-color: #00703c;
+  padding: 0px 30px;
+  overflow: auto;
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    max-height: 100%;
+
+    #close-sidebar {
+      background-color: transparent;
+      border: none;
+      margin: 12px auto 0px auto;
+      width: fit-content;
+      padding: 6px;
+    }
+
+    a {
+      font-size: 18px;
+      font-weight: bold;
+      color: #e5e5e5;
+      font-family: "Play";
+      text-decoration: none !important;
+      margin-top: 50%;
+      text-align: center;
+
+      .icon {
+        @media only screen and (min-device-width: 375px) and (max-device-width: 812px) and (-webkit-min-device-pixel-ratio: 3) {
+          width: 30%;
+        }
+      }
+    }
+  }
+}
+.slide-enter-active {
+  animation: slide-in 200ms;
+}
+.slide-leave-active {
+  animation: slide-in 200ms reverse;
+}
+@keyframes slide-in {
+  from {
+    transform: translateX(-100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+</style>
