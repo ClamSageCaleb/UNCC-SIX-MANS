c2c10fea3a5d4da6668f89ac2b224fe42811b8d8
Austin Baker <noise.9no@gmail.com>
2021-05-15 22:10:23 -0400

    Norm got his Covid-19 Vaccine

--- src/EmbedHelper.py
+++ src/EmbedHelper.py
@@ -10,7 +10,7 @@ def BaseEmbed(title: str, description: str, color: Color) -> Embed:
         description=description,
         color=color,
     ).set_thumbnail(
-        url="https://raw.githubusercontent.com/ClamSageCaleb/UNCC-SIX-MANS/master/media/norm_masked.png"
+        url="https://raw.githubusercontent.com/ClamSageCaleb/UNCC-SIX-MANS/master/media/norm_still.png"
     )
 
 
