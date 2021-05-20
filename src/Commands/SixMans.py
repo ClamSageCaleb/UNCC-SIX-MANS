1507db6b2468bf0279f2d9972619442274c25fb7
Austin Baker <noise.9no@gmail.com>
2021-05-20 00:26:50 -0400

    Requested Changes in Pull Request #25

--- src/Commands/SixMans.py
+++ src/Commands/SixMans.py
@@ -396,22 +396,22 @@ def leaderboard(author: Member, mentions: str, lbChannelId: int, *arg) -> Embed:
         edited_embed = CaptainsRandomHelpEmbed(embed, blueTeam, orangeTeam, blueCap, orangeCap)
         return edited_embed
 
-    elif (len(arg) == 0 and playerMentioned == False and Leaderboard.countLeaderboard() >= 6):
-        viewFullLb = "\nTo see the full leaderboard, visit <#{0}>.".format(lbChannelId) if (lbChannelId != -1) else ""
-        embed = InfoEmbed(
-            title="UNCC 6 Mans | Top 5",
-            desc=Leaderboard.showLeaderboard(limit=5) + viewFullLb
-        )
-        edited_embed = CaptainsRandomHelpEmbed(embed, blueTeam, orangeTeam, blueCap, orangeCap)
-        return edited_embed
-
-    elif (len(arg) == 0 and playerMentioned == False and Leaderboard.countLeaderboard() < 6):
-        embed = ErrorEmbed(
-            title="No Leaderboard!",
-            desc="There are currently no leaderboard statistics!"
-        )
-        edited_embed = CaptainsRandomHelpEmbed(embed, blueTeam, orangeTeam, blueCap, orangeCap)
-        return edited_embed
+    elif (len(arg) == 0 and playerMentioned == False):
+        if (Leaderboard.countLeaderboard() >= 6):
+            viewFullLb = "\nTo see the full leaderboard, visit <#{0}>.".format(lbChannelId) if (lbChannelId != -1) else "" # noqa
+            embed = InfoEmbed(
+                title="UNCC 6 Mans | Top 5",
+                desc=Leaderboard.showLeaderboard(limit=5) + viewFullLb
+            )
+            edited_embed = CaptainsRandomHelpEmbed(embed, blueTeam, orangeTeam, blueCap, orangeCap)
+            return edited_embed
+        else:
+            embed = ErrorEmbed(
+                title="No Leaderboard!",
+                desc="There are currently no leaderboard statistics!"
+            )
+            edited_embed = CaptainsRandomHelpEmbed(embed, blueTeam, orangeTeam, blueCap, orangeCap)
+            return edited_embed
 
     else:
         embed = ErrorEmbed(
