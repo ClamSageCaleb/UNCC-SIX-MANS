a9c506300b863d68d2464a14d2752e36b2bcbabf
Austin Baker <noise.9no@gmail.com>
2021-07-18 00:04:15 -0400

    Update to Points.py to fix bugs with MMR algorithm

--- src/Points.py
+++ src/Points.py
@@ -1,16 +1,18 @@
+from tinydb.table import Document
 from Types import Team, MatchKey
 
 
-def calculateMMR(match) -> int:
+def calculateMMR(match: Document) -> int:
     blueMMR = 0
     orangeMMR = 0
 
     for playerId in match:
-        player = match[playerId]
-        if (player[MatchKey.Team] == Team.BLUE):
-            blueMMR += player[MatchKey.MMR]
-        else:
-            orangeMMR += player[MatchKey.MMR]
+        if (playerId != MatchKey.REPORTED_WINNER):
+            player = match[playerId]
+            if (player[MatchKey.TEAM] == Team.BLUE):
+                blueMMR += player[MatchKey.MMR]
+            else:
+                orangeMMR += player[MatchKey.MMR]
 
     avgBlueMMR = blueMMR / 3
     avgOrangeMMR = orangeMMR / 3
@@ -18,17 +20,17 @@ def calculateMMR(match) -> int:
     # determine the new MMR on probability
 
     # determine the difference in MMR
-    difference = (avgOrangeMMR - avgBlueMMR) / 400
+    difference = ((avgOrangeMMR - avgBlueMMR) / 400)
 
     # take 10 to the power of our difference and make it postive by adding 1
-    power = 1 + pow(10, difference)
+    power = (pow(10, difference) + 1)
 
     # 1 divided by our positive number to determine % chance of winning
-    probability = 1 / power
+    probability = (1 / power)
 
     # 1 - our probability to start with a number greater than 1
     # 20 * our new number to enhance the amount of MMR
-    mmr = 20(1 - probability)
+    mmr = ((1 - probability) * 20)
 
     mmr = min(15, mmr)
     mmr = max(5, mmr)
