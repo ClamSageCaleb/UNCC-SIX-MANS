51d8e90c1b11ac967ca3ffde654a6f055302c9e5
Austin Baker <noise.9no@gmail.com>
2021-11-01 23:23:48 -0400

    Basic Functionallity added for Buttons

---
+++ new_src/controllers/Interactions.ts
@@ -0,0 +1,10 @@
+import { Client, Interaction, MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
+
+
+// collector.on('collect', async (i: { customId: string; update: (arg0: { content: string; components: never[]; }) => any; }) => {
+//     if (i.customId === 'joinQueue') {
+//         await i.update({ content: '${id} has been added to the queue', components: [] });
+//     }
+// });
+
+
--- new_src/index.ts
+++ new_src/index.ts
@@ -1,10 +1,62 @@
-import { Client } from "discord.js";
+import { Client, Interaction, Message, MessageActionRow, MessageButton, MessageEmbed, User } from "discord.js";
+import QueueRepository from "./repositories/QueueRepository";
 
 const NormClient = new Client({ intents: "GUILDS" });
 
 // function called on startup
-NormClient.on("ready", () => {
+NormClient.on("ready", async () => {
+  const channel = await NormClient.channels.fetch('629502331259584559')
+  if (!channel) {
+    throw new Error('Channel does not exist with id 629502331259584559');
+  }
+  if (!channel.isText()) {
+    throw new Error('Channel is not a text channel!');
+  }
+  const row = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+        .setCustomId('joinQueue')
+        .setLabel('Join')
+        .setStyle('SUCCESS')
+        .setEmoji('✅'),
+    );
+
+  const embed = new MessageEmbed()
+    .setColor('#3ba55c') // <- This is green
+    .setTitle('Queue is Empty')
+    .setDescription('Click the green button to join the queue!')
   console.info("NormJS is running.");
+
+  await channel.send({ embeds: [embed], components: [row] });
 });
+
+
+NormClient.on('interactionCreate', async (buttonInteraction: Interaction) => {
+  if (!buttonInteraction.isButton()) return;
+
+  switch (buttonInteraction.customId) {
+    case 'joinQueue': {
+      const row = new MessageActionRow()
+        .addComponents(
+          new MessageButton()
+            .setCustomId('joinQueue')
+            .setLabel('Join')
+            .setStyle('SUCCESS')
+            .setEmoji('✅'),
+        );
+
+      const embed = new MessageEmbed()
+        .setColor('#3ba55c') // <- This is green
+        .setTitle(buttonInteraction.user.username + ' Joined the Queue!')
+        .setDescription('Click the green button to join the queue!\n\n' +
+        await QueueRepository.getAllBallChasersInQueue() + ' h')
+
+      //await QueueRepository.addBallChaserToQueue(buttonInteraction.user)
+      await buttonInteraction.update({ embeds: [embed] })
+    }
+
+    break;
+  }
+})
 
 NormClient.login(process.env.token);
