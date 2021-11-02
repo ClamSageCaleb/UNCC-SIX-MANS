461d8702645c090cf3af20b86fe16dd5022ae6d3
Austin Baker <noise.9no@gmail.com>
2021-11-02 13:02:28 -0400

    Rearrangement of code for readability -Introduction of new error, when clicking Join button

--- new_src/index.ts
+++ new_src/index.ts
@@ -37,10 +37,44 @@ NormClient.on("ready", async () => {
 
 
 NormClient.on('interactionCreate', async (buttonInteraction: Interaction) => {
+  let fullQueue = new Boolean(false);
   if (!buttonInteraction.isButton()) return;
 
   switch (buttonInteraction.customId) {
     case 'joinQueue': {
+
+      //await QueueRepository.addBallChaserToQueue(buttonInteraction.user)
+
+      const row = new MessageActionRow()
+        .addComponents(
+          new MessageButton()
+            .setCustomId('joinQueue')
+            .setLabel('Join')
+            .setStyle('SUCCESS')
+            .setEmoji('✅'),
+            //.setDisabled(Need a method that returns T/F to determine if this is a clickable button after a full queue)
+          new MessageButton()
+            .setCustomId('leaveQueue')
+            .setLabel('Leave')
+            .setStyle('DANGER')
+            .setEmoji('❌'),
+        );
+
+      const embed = new MessageEmbed()
+        .setColor('#3ba55c') // <- This is green
+        .setTitle(buttonInteraction.user.username + ' Joined the Queue!')
+        .setDescription('Click the green button to join the queue!\n\n' +
+          await QueueRepository.getAllBallChasersInQueue() + ' h')
+
+      await buttonInteraction.update({ embeds: [embed], components: [row] })
+    }
+    
+    // Introduction of new error here after adding leave button. Join is called twice on the same interaction
+    // Error [INTERACTION_ALREADY_REPLIED]: The reply to this interaction has already been sent or deferred.
+    case 'leaveQueue': {
+
+      //await QueueRepository.removeBallChaserFromQueue(buttonInteraction.user)
+      
       const row = new MessageActionRow()
         .addComponents(
           new MessageButton()
@@ -57,40 +91,14 @@ NormClient.on('interactionCreate', async (buttonInteraction: Interaction) => {
 
       const embed = new MessageEmbed()
         .setColor('#3ba55c') // <- This is green
-        .setTitle(buttonInteraction.user.username + ' Joined the Queue!')
-        .setDescription('Click the green button to join the queue!\n\n' +
-        await QueueRepository.getAllBallChasersInQueue() + ' h')
-
-      //await QueueRepository.addBallChaserToQueue(buttonInteraction.user)
-      await buttonInteraction.update({ embeds: [embed] })
-    }
-
-    case 'leaveQueue': {
-      const row = new MessageActionRow()
-        .addComponents(
-          new MessageButton()
-            .setCustomId('joinQueue')
-            .setLabel('Join')
-            .setStyle('SUCCESS')
-            .setEmoji('✅'),
-          new MessageButton()
-            .setCustomId('leaveQueue')
-            .setLabel('Leave')
-            .setStyle('DANGER')
-            .setEmoji('❌'),
-        );
-
-      const embed = new MessageEmbed()
-        .setColor('#ed4245') // <- This is red
         .setTitle(buttonInteraction.user.username + ' Left the Queue!')
         .setDescription('Click the green button to join the queue!\n\n' +
-        await QueueRepository.getAllBallChasersInQueue() + ' h')
+          await QueueRepository.getAllBallChasersInQueue() + ' h')
 
-      //await QueueRepository.removeBallChaserFromQueue(buttonInteraction.user)
-      await buttonInteraction.update({ embeds: [embed] })
+      await buttonInteraction.update({ embeds: [embed], components: [row] })
     }
-    break;
-    
+      break;
+
   }
 })
 
