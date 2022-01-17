0f7a0132d968bb3930ac55ed2521299858a7579d
Matt Wells <mattwells878@gmail.com>
2022-01-17 16:07:04 -0500

    Add Fall 2021 Winners (#50)
    * Add winners
    
    * Add MMR column to leaderboard table

--- github_pages_files/js/main.js
+++ github_pages_files/js/main.js
@@ -4,40 +4,41 @@ $(function () {
     url: "https://uncc-six-mans.s3.amazonaws.com/Leaderboard.json",
     dataType: "json",
     success: (data) => populateTable(data),
-  })
-  setScrollBar()
-  $(window).resize(setScrollBar)
-  $("#lbSearch").keyup((e) => filterLeaderboard(e.target.value))
-})
+  });
+  setScrollBar();
+  $(window).resize(setScrollBar);
+  $("#lbSearch").keyup((e) => filterLeaderboard(e.target.value));
+});
 
 function populateTable(ballChasers, query = null) {
-  $("#leaderboard-table > tbody").empty()
+  $("#leaderboard-table > tbody").empty();
   ballChasers.forEach((ballChaser, index) => {
     if (!query || ballChaser["Name"].toLowerCase().includes(query.toLowerCase())) {
       $("#leaderboard-table > tbody").append(
         `<tr>
-              <td headers="rank">${index + 1}</td>
-              <td headers="name">${ballChaser["Name"]}</td>
-              <td headers="wins">${ballChaser["Wins"]}</td>
-              <td headers="losses">${ballChaser["Losses"]}</td>
-              <td headers="matches-played">${ballChaser["Matches Played"]}</td>
-              <td headers="win-perc">${parseInt(ballChaser["Win Perc"] * 100)}%</td>
+          <td headers="rank">${index + 1}</td>
+          <td headers="name">${ballChaser["Name"]}</td>
+          <td headers="name">${ballChaser["MMR"]}</td>
+          <td headers="wins">${ballChaser["Wins"]}</td>
+          <td headers="losses">${ballChaser["Losses"]}</td>
+          <td headers="matches-played">${ballChaser["Matches Played"]}</td>
+          <td headers="win-perc">${parseInt(ballChaser["Win Perc"] * 100)}%</td>
           </tr>`
-      )
+      );
     }
-  })
+  });
   // this prevents page jumping when filtering
-  $("#leaderboard").css("min-height", $("#leaderboard").height())
+  $("#leaderboard").css("min-height", $("#leaderboard").height());
 }
 
 function setScrollBar() {
-  $("#content").css("height", window.innerHeight - $("header")[0].getBoundingClientRect().height)
+  $("#content").css("height", window.innerHeight - $("header")[0].getBoundingClientRect().height);
 }
 
 function filterLeaderboard(query) {
   $("#leaderboard-table > tbody  > tr").each((_, tr) => {
-    const playerName = $(tr).children()[1].innerText.toLowerCase()
-    if (playerName.includes(query.toLowerCase())) $(tr).show("fade")
-    else $(tr).hide("fade")
-  })
+    const playerName = $(tr).children()[1].innerText.toLowerCase();
+    if (playerName.includes(query.toLowerCase())) $(tr).show("fade");
+    else $(tr).hide("fade");
+  });
 }
--- github_pages_files/js/winners.js
+++ github_pages_files/js/winners.js
@@ -1,26 +1,29 @@
-$(() => loadWinners())
+$(() => loadWinners());
 
-const dataBaseUrl = "https://uncc-six-mans.s3.amazonaws.com/winners"
-const winnerFiles = ["summer2020.json", "fall2020.json", "spring2021.json", "summer2021.json"]
+const dataBaseUrl = "https://uncc-six-mans.s3.amazonaws.com/winners";
+const winnerFiles = ["summer2020.json", "fall2020.json", "spring2021.json", "summer2021.json", "fall2021.json"];
 
 const loadWinners = async () => {
   for (const file of winnerFiles) {
-    const data = await fetch(`${dataBaseUrl}/${file}`).then((res) => res.json())
-    data.forEach((player, index) => populateWinners(player, index + 1, file.replace(".json", "")))
+    const data = await fetch(`${dataBaseUrl}/${file}`).then((res) => res.json());
+    data.forEach((player, index) => populateWinners(player, index + 1, file.replace(".json", "")));
   }
 
   $("#accordion").accordion({
     collapsible: true,
     icons: { header: "accordionClosed", activeHeader: "accordionOpen" },
     heightStyle: "content",
-  })
-}
+  });
+};
 
 function populateWinners(winnerDetails, rank, tableId) {
+  const mmrRow = "MMR" in winnerDetails ? `<td headers="${tableId}-mmr">${winnerDetails["MMR"]}</td>` : null;
+
   $(`#winners-table-${tableId} > tbody`).append(
     `<tr>
       <td headers="${tableId}-rank">${rank}</td>
       <td headers="${tableId}-name">${winnerDetails["Name"]}</td>
+      ${mmrRow}
       <td headers="${tableId}-wins">${winnerDetails["Wins"]}</td>
       <td headers="${tableId}-losses">${winnerDetails["Losses"]}</td>
       <td headers="${tableId}-matches-played">${winnerDetails["Wins"] + winnerDetails["Losses"]}</td>
@@ -28,5 +31,5 @@ function populateWinners(winnerDetails, rank, tableId) {
       (winnerDetails["Wins"] / (winnerDetails["Wins"] + winnerDetails["Losses"])) * 100
     )}%</td>
     </tr>`
-  )
+  );
 }
